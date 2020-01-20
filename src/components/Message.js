f40fce1c07927270af6a4ed9b514babddffc709f
Katiuska Franco <kati.franco@gmail.com>
2020-01-20 17:59:28 -0500

    estilos
    Algo más de estilos

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import '../assets/styles/components/Message.scss';
 
 function Message({ text }) {
+
 return (<div className="Message">{text}</div>);
+
 }
 
 Message.propTypes = {
